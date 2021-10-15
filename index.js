0eb54559364b2b34818b97e8243bd5800d4b3038
Svetlana <svetlanaarkova7@gmail.com>
2021-10-15 13:49:35 +0300

    реализовано добавление и удаление

--- index.js
+++ index.js
@@ -6,27 +6,28 @@ document.addEventListener("DOMContentLoaded",()=>{
         console.log(document.getElementById("input").value);
 
         const cross = document.createElement("button");
-        cross.innerHTML="&#10006;";
-
-        console.log(1,counter);
-        const innerCounter = counter;
-        cross.addEventListener("click",()=>{
-            let el = document.getElementById(innerCounter.toString());
-            el.parentNode.removeChild(el);
-        })
-
         const addCheckbox = document.createElement("input");
-        addCheckbox.type="checkbox";
         const text = document.createTextNode(document.getElementById("input").value);
         const newElement = document.createElement("li");
+        const innerCounter = counter;
+        let txt = document.getElementById("input");
+
+        cross.innerHTML="&#10006;";
+        addCheckbox.type="checkbox";
+
+
         newElement.id = counter.toString();
         counter++;
         newElement.append(addCheckbox);
         newElement.append(text);
         newElement.append(cross);
         document.getElementById("list").appendChild(newElement);
-        let txt = document.getElementById("input");
         input.value = "";
+
+        cross.addEventListener("click",()=>{
+            let el = document.getElementById(innerCounter.toString());
+            el.parentNode.removeChild(el);
+        })
     })
 
 
